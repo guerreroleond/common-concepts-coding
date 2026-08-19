47998ea2accf0e04e3e9611a054c60f999578cca
guerreroleond <guerreroleond@gmail.com>
2026-08-18 21:10:02 -0600

    Implement ContainsDuplicate class to check for duplicate values in an integer array (#1)

--- CommonConceptsCoding.Console/Program.cs
+++ CommonConceptsCoding.Console/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CommonConceptsCoding.Console;
+
+Console.WriteLine("Common Concepts for Coding");
+
+#region 01Arrays&Hashing-01ContainsDuplicate
+
+var containsDuplicate = new ContainsDuplicate();
+
+int[] numsWithDuplicate = [1, 2, 3, 1];
+int[] numsWithoutDuplicate = [1, 2, 3, 4];
+
+Console.WriteLine("Contains Duplicates?");
+Console.WriteLine($"[1,2,3,1] -> {containsDuplicate.Solve(numsWithDuplicate)}");
+Console.WriteLine($"[1,2,3,4] -> {containsDuplicate.Solve(numsWithoutDuplicate)}");
+
+#endregion 01Arrays&Hashing-01ContainsDuplicate
