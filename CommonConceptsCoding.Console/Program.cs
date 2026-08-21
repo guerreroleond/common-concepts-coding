ee73f96d88af94672c0329b3d9a656fb7840a3e4
David Guerrero <guerreroleond@gmail.com>
2026-08-20 21:38:31 -0600

    Add ValidAnagram class to check if two strings are anagrams

--- CommonConceptsCoding.Console/Program.cs
+++ CommonConceptsCoding.Console/Program.cs
@@ -5,11 +5,21 @@ Console.WriteLine("Common Concepts for Coding");
 
 #region 01Arrays&Hashing-01ContainsDuplicate
 
-int[] numsWithDuplicate = [1, 2, 3, 1];
-int[] numsWithoutDuplicate = [1, 2, 3, 4];
+//int[] numsWithDuplicate = [1, 2, 3, 1];
+//int[] numsWithoutDuplicate = [1, 2, 3, 4];
 
-Console.WriteLine("Contains Duplicates?");
-Console.WriteLine($"[1,2,3,1] -> {ContainsDuplicate.Solve(numsWithDuplicate)}");
-Console.WriteLine($"[1,2,3,4] -> {ContainsDuplicate.Solve(numsWithoutDuplicate)}");
+//Console.WriteLine("Contains Duplicates?");
+//Console.WriteLine($"[1,2,3,1] -> {ContainsDuplicate.Solve(numsWithDuplicate)}");
+//Console.WriteLine($"[1,2,3,4] -> {ContainsDuplicate.Solve(numsWithoutDuplicate)}");
 
 #endregion 01Arrays&Hashing-01ContainsDuplicate
+
+#region 01Arrays&Hashing-02ValidAnagram
+
+var validAnagram = new ValidAnagram();
+
+Console.WriteLine("Valid Anagram?");
+Console.WriteLine($"anagram, nagaram -> {ValidAnagram.Solve("anagram", "nagaram")}");
+Console.WriteLine($"rat, car -> {ValidAnagram.Solve("rat", "car")}");
+
+#endregion 01Arrays&Hashing-02ValidAnagram
